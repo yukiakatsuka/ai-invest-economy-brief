cefca58162971389ec7662f10d30e3bb9babb468
yukiakatsuka <155287213+yukiakatsuka@users.noreply.github.com>
2026-06-14 11:22:25 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "brief-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./src/styles.css",
+  "./src/app.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
